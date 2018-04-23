43d098caaaafdb30aaefb614f3d3f3c6ffdd76ab
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using DevExpress.Web.ASPxEditors;
+using DevExpress.Web.ASPxFormLayout;
+using DevExpress.Web.ASPxPager;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    
+    protected void ASPxFormLayout4_LayoutItemDataBound(object sender, DevExpress.Web.ASPxFormLayout.LayoutItemDataBoundEventArgs e)
+    {
+        Control c = e.LayoutItem.GetNestedControl();
+        if (c is ASPxBinaryImage)
+        {
+            ASPxBinaryImage img = (ASPxBinaryImage)c;
+            DataView view = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            DataTable tb = view.ToTable();
+            img.Value = ImageHelper.ConvertOleObjectToByteArray(tb.Rows[0]["Picture"]);
+            img.ToolTip = tb.Rows[0]["Description"].ToString();
+        }
+    }
+    protected void ASPxPager1_Init(object sender, EventArgs e)
+    {
+        DataView view = (DataView)sdRecordsCount.Select(DataSourceSelectArguments.Empty);
+        ((ASPxPager)sender).ItemCount = (int)view.Table.Rows[0][0];
+    }
+    protected void ASPxPager1_PageIndexChanging(object source, PagerPageEventArgs e)
+    {
+        SqlDataSource1.SelectParameters["ProductID"].DefaultValue = (e.NewPageIndex + 1).ToString();
+    }
+    protected void UpdButton_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            DataView view = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            DataTable tb = view.ToTable();
+            SqlDataSource1.UpdateParameters["ProductID"].DefaultValue = tb.Rows[0]["ProductID"].ToString();
+            SqlDataSource1.Update();
+        }
+        catch (SqlException ex)
+        {
+            errorMessageLabel.Text = ex.Message;
+            errorMessageLabel.Visible = true;
+        }
+    }
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,46 @@
+﻿Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxFormLayout
+Imports DevExpress.Web.ASPxPager
+Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Linq
+Imports System.Text
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub ASPxFormLayout4_LayoutItemDataBound(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxFormLayout.LayoutItemDataBoundEventArgs)
+        Dim c As Control = e.LayoutItem.GetNestedControl()
+        If TypeOf c Is ASPxBinaryImage Then
+            Dim img As ASPxBinaryImage = CType(c, ASPxBinaryImage)
+            Dim view As DataView = DirectCast(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+            Dim tb As DataTable = view.ToTable()
+            img.Value = ImageHelper.ConvertOleObjectToByteArray(tb.Rows(0)("Picture"))
+            img.ToolTip = tb.Rows(0)("Description").ToString()
+        End If
+    End Sub
+    Protected Sub ASPxPager1_Init(ByVal sender As Object, ByVal e As EventArgs)
+        Dim view As DataView = DirectCast(sdRecordsCount.Select(DataSourceSelectArguments.Empty), DataView)
+        DirectCast(sender, ASPxPager).ItemCount = CInt((view.Table.Rows(0)(0)))
+    End Sub
+    Protected Sub ASPxPager1_PageIndexChanging(ByVal source As Object, ByVal e As PagerPageEventArgs)
+        SqlDataSource1.SelectParameters("ProductID").DefaultValue = (e.NewPageIndex + 1).ToString()
+    End Sub
+    Protected Sub UpdButton_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Try
+            Dim view As DataView = DirectCast(SqlDataSource1.Select(DataSourceSelectArguments.Empty), DataView)
+            Dim tb As DataTable = view.ToTable()
+            SqlDataSource1.UpdateParameters("ProductID").DefaultValue = tb.Rows(0)("ProductID").ToString()
+            SqlDataSource1.Update()
+        Catch ex As SqlException
+            errorMessageLabel.Text = ex.Message
+            errorMessageLabel.Visible = True
+        End Try
+    End Sub
+End Class
